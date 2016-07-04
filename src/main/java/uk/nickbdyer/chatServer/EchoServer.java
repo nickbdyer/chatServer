d5d4fdf152c4da1016ff3342d8c340b715ae5aa1
Nick Dyer <nbdyer@gmail.com>
2016-07-04 21:01:15 +0100

    Changes echo server to be 'chat server' as per feature request

--- src/main/java/uk/nickbdyer/chatServer/EchoServer.java
+++ src/main/java/uk/nickbdyer/chatServer/EchoServer.java
@@ -48,14 +48,12 @@ public class EchoServer {
                 ServerSocket serverSocket =
                         new ServerSocket(Integer.parseInt(args[0]));
                 Socket clientSocket = serverSocket.accept();
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(clientSocket.getInputStream()));
         ) {
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
+                System.out.println(inputLine);
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
