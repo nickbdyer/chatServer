a70ff642fb5601c644de8769cf2e0ff3863991c1
Nick Dyer <nbdyer@gmail.com>
2016-07-05 15:03:04 +0100

    Adds test for client socket connection to server

--- src/test/java/uk/nickbdyer/chatserver/ChatServerTest.java
+++ src/test/java/uk/nickbdyer/chatserver/ChatServerTest.java
@@ -1,12 +1,28 @@
 package uk.nickbdyer.chatserver;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import static org.junit.Assert.assertTrue;
 
 public class ChatServerTest {
+
+    private ServerSocket serverSocket;
+
+    @Before
+    public void setUp() throws IOException {
+        serverSocket = new ServerSocket(4440);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        serverSocket.close();
+    }
 
     @Test
     public void whenListeningForConnectionsTheServerSocketWillHaveAcceptCalled() throws IOException {
@@ -19,18 +35,25 @@ public class ChatServerTest {
     }
 
     @Test(expected=RuntimeException.class)
-    public void ifTheServerSocketCannotAcceptConnectionsARunTimeExceptionWillBeThrown() {
-        FaultyServerSocket mockServerSocket = null;
-        try {
-            mockServerSocket = new FaultyServerSocket();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void ifTheServerSocketCannotAcceptConnectionsARunTimeExceptionWillBeThrown() throws IOException {
+        FaultyServerSocket mockServerSocket = new FaultyServerSocket();
         ChatServer chatServer = new ChatServer(mockServerSocket);
 
         chatServer.listen();
     }
+    
+    @Test
+    public void aClientSocketCanConnectToTheServer() throws IOException, InterruptedException {
+        Thread serverThread = new Thread(() -> {
+            ChatServer chatServer = new ChatServer(serverSocket);
+            chatServer.listen();
+        });
+        serverThread.start();
+        Socket clientSocket = new Socket("localhost", 4440);
+        serverThread.join();
 
+        assertTrue(clientSocket.isConnected());
+    }
 
 
 }
