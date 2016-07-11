6e62b6c3fdf98723c4a3c785d54c26b4e7b89a93
Nick Dyer <nbdyer@gmail.com>
2016-07-11 14:11:37 +0100

    Change names of validator to arguments, since it is really providing rather than validating

--- src/main/java/uk/nickbdyer/chatserver/Arguments.java
+++ src/main/java/uk/nickbdyer/chatserver/Arguments.java
@@ -1,8 +1,8 @@
 package uk.nickbdyer.chatserver;
 
-public class UserInputValidator {
+public class Arguments {
 
-    public static Integer getPortNumber(String[] args) {
+    public static Integer getPort(String[] args) {
         if(args.length != 1) return 4444;
         try {
             return Integer.parseInt(args[0]);
--- src/main/java/uk/nickbdyer/chatserver/Main.java
+++ src/main/java/uk/nickbdyer/chatserver/Main.java
@@ -6,7 +6,8 @@ import java.net.ServerSocket;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new ChatServer(new ServerSocket(UserInputValidator.getPortNumber(args)), System.out).acceptConnections();
+        ServerSocket server = new ServerSocket(Arguments.getPort(args));
+        new ChatServer(server, System.out).acceptConnections();
     }
 
 }
