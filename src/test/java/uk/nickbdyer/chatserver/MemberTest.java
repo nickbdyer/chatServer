d4d76caa138270e3cf186683e3a4161801eb709b
Nick Dyer <nbdyer@gmail.com>
2016-07-29 14:56:42 +0100

    Message send by one member can be heard by another user in the chatroom

--- src/test/java/uk/nickbdyer/chatserver/MemberTest.java
+++ src/test/java/uk/nickbdyer/chatserver/MemberTest.java
@@ -1,10 +1,12 @@
 package uk.nickbdyer.chatserver;
 
 import org.junit.Test;
+import uk.nickbdyer.chatserver.testdoubles.FakeUser;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MemberTest {
 
@@ -17,4 +19,17 @@ public class MemberTest {
 
         assertEquals(chatRoom.messages(), Arrays.asList("Hi everyone!"));
     }
+
+    @Test
+    public void aMessageSentToTheChatRoomIsHeardByAnotherUser() {
+        Member nick = new Member();
+        FakeUser testUser = new FakeUser();
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.addUser(testUser);
+
+        nick.sendMessageToRoom("Hi everyone!", chatRoom);
+
+        assertTrue(testUser.receivedMessage("Hi everyone!"));
+    }
+
 }
