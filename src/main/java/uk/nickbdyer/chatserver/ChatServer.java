ad32f2c704d82af1f96447bae35ba05c65f0a5db
Nick Dyer <nbdyer@gmail.com>
2016-07-31 18:18:58 +0100

    Removes methods that have now been removed from tests

--- src/main/java/uk/nickbdyer/chatserver/ChatServer.java
+++ src/main/java/uk/nickbdyer/chatserver/ChatServer.java
@@ -1,9 +1,7 @@
 package uk.nickbdyer.chatserver;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.Executor;
 
@@ -12,30 +10,12 @@ public class ChatServer {
     private final ChatRoom chatRoom;
     private final Executor executor;
     private ServerSocket serverSocket;
-    private OutputStream output;
     private boolean listening;
-
-    public ChatServer(ChatRoom chatRoom, ServerSocket serverSocket, OutputStream receivedMessage) {
-        this.chatRoom = chatRoom;
-        this.serverSocket = serverSocket;
-        this.output = receivedMessage;
-        executor = null;
-    }
 
     public ChatServer(Executor executor, ServerSocket serverSocket, ChatRoom chatRoom) {
         this.serverSocket = serverSocket;
         this.chatRoom = chatRoom;
         this.executor = executor;
-    }
-
-    public void listen() {
-        try {
-            Socket clientSocket = serverSocket.accept();
-            chatRoom.addUser(new Member());
-            new MessageStream(clientSocket.getInputStream(), output).send();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void start() {
